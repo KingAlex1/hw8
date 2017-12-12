0bfcb79cf53d69edd1bae6cf7d8e196c4d24477a
Alex Bogomolov <alex-sert2015@mail.ru>
2017-12-12 05:02:13 +0400

    add travis and deploy

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import rootReducer from './reducers';
-import rootSaga from './sagas';
-import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import rootSaga from "./sagas";
+import createSagaMiddleware from "redux-saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
